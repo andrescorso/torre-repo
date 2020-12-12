85a3b9a6b652401dee9a855958cbf191dafee128
andrescorso <andres.corso+git@gmail.com>
2020-12-12 18:36:59 -0500

    Initial Vue App commit

---
+++ torre-comparasion/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="blue-grey darken-4"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/logo.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Code</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ torre-comparasion/src/Config.js
@@ -0,0 +1,4 @@
+export var Global = {
+  "infoAPI": "https://torre.co/api/",
+  "searchAPI": "https://search.torre.co/people/_search/?currency=USD%24&page=0&periodicity=hourly&lang=es&size=3&aggregate=false&offset=0"
+}
